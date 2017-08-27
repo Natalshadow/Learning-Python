8969a042d1759d772b813f9dd7e8a84b9a185ec5
Unknown <gauthier.menin@gmail.com>
2017-08-27 15:48:50 +0200

    TicTacToe
    Attempting the TicTacToe from Automate Boring Stuff

--- Test.py
+++ Test.py
@@ -1,1 +1,21 @@
+import sys
+import pprint
+import random
 
+def tictactoe():
+    board = {'topLeft': ' ',
+             'topRight': ' ',
+             'topMid': ' ',
+             'midLeft': ' ',
+             'midRight': ' ',
+             'midMid': ' ',
+             'botLeft': ' ',
+             'botRight': ' ',
+             'botMid': ' '}
+
+    print(board)
+
+def printBoard():
+
+
+tictactoe()
