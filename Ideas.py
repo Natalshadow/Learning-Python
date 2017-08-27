7f8ba0d06ab8e0cb4258c3919c781115a15f268d
Unknown <gauthier.menin@gmail.com>
2017-08-27 15:35:44 +0200

    Added Ideas.py
    Ideas.py contains short codes that might trigger ideas of games and projects later

---
+++ Ideas.py
@@ -0,0 +1,22 @@
+#Here I will store chunk of codes that are currently useless but might give me ideas later
+
+import sys
+import calendar
+import time
+import datetime
+
+def locatairesdatetimes():
+    arrival = datetime.date(2016,8,1)
+    departure = datetime.date(2017,1,20)
+    test = departure - arrival
+    print('%s - %s = %s' %(arrival, departure, test))
+
+    Fanny = {'Arrival': datetime.date(2016,8,1), 'Departure': datetime.date(2017,1,20), 'Rent': 440}
+
+    duration = (Fanny['Departure'] - Fanny['Arrival']) / 30
+    incomes = duration * Fanny['Rent']
+
+    print('Duration')
+    print(duration)
+    print('Incomes')
+    print(incomes)
