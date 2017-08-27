94b9a099758129f6e6165a9750059d86cf1fb28c
Unknown <gauthier.menin@gmail.com>
2017-08-27 14:46:25 +0200

    Playing with lists again

--- Test.py
+++ Test.py
@@ -1,3 +1,24 @@
+# Adding user input to a list
+import sys
+
+def list_try():
+    liste = []
+
+    while True:
+        print('List trial, input items: ')
+        item = input()
+        item.lower()
 
 
-print(datetime.datetime.now())
+        if item == "exit":
+            print('Entered exit, exiting.')
+            sys.exit()
+
+        elif item == "print":
+            print(liste)
+
+        else:
+            liste.append(item)
+
+
+list_try()
